2401cacea06220d3b1a3dc2667b52c55a99d84f5
Narawit Khammanart <tarantia.sg@gmail.com>
2023-02-10 22:24:51 +0700

    Init

---
+++ src/main/java/org/narawit/comledger/coreapi/ComputerLedgerCoreApiApplication.java
@@ -0,0 +1,13 @@
+package org.narawit.comledger.coreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerLedgerCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerLedgerCoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/narawit/comledger/coreapi/ComputerLedgerCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.narawit.comledger.coreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerLedgerCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
